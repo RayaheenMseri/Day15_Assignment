21ac1513dd1bc800bbcea70f8e1012f64e97bdff
RayaheenMseri <rayaheenmseri@gmail.com>
2025-03-25 11:17:34 +0300

    Initial Commit

---
+++ authProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  authProject
+//
+//  Created by Rayaheen Mseri on 25/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ authProject/authProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  authProjectApp.swift
+//  authProject
+//
+//  Created by Rayaheen Mseri on 25/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct authProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
